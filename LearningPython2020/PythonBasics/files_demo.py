bacd10a0f401623aeb68898427fa04f291ecc0a3
jb087893 <john.barry@cerner.com>
2022-01-21 15:22:30 -0600

    demo functions for files
    demo functions for reading, writing, appending to text files

--- LearningPython2020/PythonBasics/files_demo.py
+++ LearningPython2020/PythonBasics/files_demo.py
@@ -3,3 +3,41 @@
 # - Reading and Writing Files 
 # - OS Path   
 # --------------------------------
+
+# Writing Data to a Text File
+def write_demo():
+    # open a file or creating if it doesn't exist
+    my_file = open("demofile.txt", "w+")
+
+    # write lines to the demo text file
+    for n in range(5):
+        my_file.write(f"This is line {n+1} \n")
+
+    # close the file when done 
+    my_file.close()
+
+# Appending Data to a Text File 
+def append_demo():
+    # open file we want to append to 
+    my_file = open("demofile.txt", "a")
+    
+    # write lines to append to the demo text file 
+    for n in range(5):
+        my_file.write(f"The is an appended line {n+1}\n")
+
+    # close the file when done 
+    my_file.close()
+
+# Reading Data from a Text File 
+def read_demo():
+    # try to open file for read access 
+    try: 
+        my_file = open("demofile.txt", "r")
+        # read lines in file
+        lines = my_file.readlines()
+        for line in lines: 
+            print(line)
+        my_file.close()
+    except: 
+        print("Cannot find demo file")
+
